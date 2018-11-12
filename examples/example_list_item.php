8f58dede0e93894731e4b642a035d32ae7c63feb
Damian Gabriel Torres <42480571+papeleradamian@users.noreply.github.com>
2018-11-12 18:22:55 -0300

    Update example_list_item.php

--- examples/example_list_item.php
+++ examples/example_list_item.php
@@ -9,12 +9,12 @@ $meli = new Meli($appId, $secretKey);
 if($_GET['code']) {
 
 	// If the code was in get parameter we authorize
-	$user = $meli->authorize($_GET['code'], $redirectURI);
+	$user = $meli;
 
 	// Now we create the sessions with the authenticated user
-	$_SESSION['access_token'] = $user['body']->access_token;
-	$_SESSION['expires_in'] = $user['body']->expires_in;
-	$_SESSION['refresh_token'] = $user['body']->refresh_token;
+	$_SESSION['access_token'] = $user['body'];
+	$_SESSION['expires_in'] = $user['body'];
+	$_SESSION['refresh_token'] = $user['body'];
 
 	// We can check if the access token in invalid checking the time
 	if($_SESSION['expires_in'] + time() + 1 < time()) {
